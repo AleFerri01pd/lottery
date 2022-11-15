1e01eee13032397033fff40d94bc7086c4614c98
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-11-15 17:47:35 +0100

    first commit

---
+++ deploy.js
@@ -0,0 +1,32 @@
+// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+provider = new HDWalletProvider(
+    'heavy zoo announce assist cluster blue tourist design place tray art cry', //Secret Recovery Phrase di Metamask
+    'https://goerli.infura.io/v3/610553932f2747d189a138d911305bf3'  
+);
+const web3 = new Web3(provider);
+const prompt = require("prompt-sync")({ sigint: true });
+const INITIAL_STRING = prompt("Set Initial Message: ");
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Account' , accounts[0]);
+
+    const result = await new web3.eth.Contract(abi) //interface è ABI, oggetto JS del contratto
+    .deploy({ 
+        data: evm.bytecode.object, //codice binario per distribuire il contratto sulla rete ETH
+        arguments: [INITIAL_STRING]
+    })
+    .send({
+        from: accounts[0],
+        gas: '1000000'
+    });
+
+    console.log('Contract deployed at: ', result.options.address);
+    provider.engine.stop();
+}
+
+deploy();
